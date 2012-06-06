aa6adf4e50d5b055ecb8e0d6420ae11d13cdd216
Clément <clmntlxndr@gmail.com>
2012-06-06 20:25:04 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Texterest::Application.config.secret_token = 'f4fb145138d59d84c70efcc95c513b004eabfadff030f1b94ed24bb10cfdd83eb46d6b310f5edcc2308bc4cda7a795f46942109043b529abd957510041aa4334'
